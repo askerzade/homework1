e65d4392b7e603c61ac9379f9136d369d1e62927
askerzade <pedro100500@gmail.com>
2019-10-17 00:54:27 +0400

    \\homework1

---
+++ src/main/java/homework1/homework1.java
@@ -0,0 +1,22 @@
+package homework1;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class homework1 {
+    public static void main(String[] args) {
+
+        Scanner std = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        String name = std.nextLine();
+        System.out.println("Your Name Is: " + name);
+        System.out.println("Let the Game Begins!");
+        int[] a = new int[100];
+        int r = a.length;
+
+
+
+    }
+
+
+    }
