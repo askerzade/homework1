ee4345372ad0a70e21adb04b47264f121316f3e4
askerzade <pedro100500@gmail.com>
2019-10-17 14:40:24 +0500

    --name

--- src/main/java/homework1/homework1.java
+++ src/main/java/homework1/homework1.java
@@ -1,10 +1,12 @@
 package homework1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 
 public class homework1 {
     public static void main(String[] args) {
+        Random random = new Random();
 
         Scanner std = new Scanner(System.in);
         System.out.print("Enter Your Name: ");
@@ -14,9 +16,23 @@ public class homework1 {
         int[] a = new int[100];
         int r = a.length;
 
+        int rnd1 = random.nextInt(r);
+        System.out.println("Write your number: ");
+        Scanner x = new Scanner(System.in);
+        int pn = x.nextInt();
+
+        if (rnd1 == pn){
+            System.out.println("Congratulations! You WON!");
+        }
+        if (rnd1<= pn){
+            System.out.println("Your number is bigger. Please try again");
+        }
 
 
-    }
+
+    }}
 
 
-    }
+
+
+
